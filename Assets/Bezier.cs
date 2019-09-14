7283d474e07f5e2b7ba9eea351997b7f7360e801
Nejc Anclin <anclin@MacBook-Pro-Anclin-2.local>
2019-09-14 20:03:45 +0200

    Camera changes settings when close to bezier rail.

--- Assets/Bezier.cs
+++ Assets/Bezier.cs
@@ -2,67 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class BezierPoint {
+    public Bezier Bezier;
+    public Vector3 Position;
+    public int SegmentIdx;
+
+    public BezierPoint(Bezier bezier, Vector3 p, int segmentIdx) {
+        Bezier = bezier;
+        Position = p;
+        SegmentIdx = segmentIdx;
+    }
+
+    public Vector3 SegmentP0 { get { return Bezier.GetBakedPoints[SegmentIdx]; } }
+    public Vector3 SegmentP1 { get { return Bezier.GetBakedPoints[SegmentIdx + 1]; } }
+}
+
 public class Bezier : MonoBehaviour {
 
     [SerializeField] private bool ShowGizmos = true;
-    [SerializeField] private Transform RefPoint = null;
     [SerializeField] private List<Transform> Points = new List<Transform>();
-    [SerializeField] [Range(3, 20)] private int SampleCount = 5;
+    [SerializeField] [Range(2, 20)] private int SampleCount = 5;
 
-    [SerializeField] private List<Vector3> SamplePoints = new List<Vector3>();
+    [SerializeField] private List<Vector3> BakedPoints = new List<Vector3>();
 
-    Vector3 ClosestPoint;
-    Vector3 sA;
-    Vector3 sB;
-    int ClosestIndex;
+    public List<Transform> GetPoints { get { return Points; } }
+    public List<Vector3> GetBakedPoints { get { return BakedPoints; } }
 
-    public List<Vector3> GetClosestPoint() {
-        return new List<Vector3>(){
-            ClosestPoint,
-            sA,
-            sB
-        };
+    BezierPoint ClosestPoint;
+
+    public void BakeBezier() {
+        BakedPoints = new List<Vector3>();
+        int segmentCount = (Points.Count + 2) / 3 - 1;
+        for (int i = 0; i < SampleCount * segmentCount; i++) {
+            float t = (float)i / (float)(SampleCount * segmentCount - 1);
+            BakedPoints.Add(PointOnBezier(t));
+        }
     }
 
-    public List<Vector3> GetSamplePoints{ get { return SamplePoints; }}
-
-	private void Update() {
-        SamplePoints = new List<Vector3>();
-        for (int i = 0; i < SampleCount; i++)
-        {
-            float t = (float)i / (float)(SampleCount-1);
-            SamplePoints.Add(PointOnBezier(t));
-        }
-
-        Vector3 P = RefPoint.position;
+    public BezierPoint GetClosestPoint(Vector3 P) {
         float closestDistance = Mathf.Infinity;
 
-        for (int i = 0; i < SamplePoints.Count-1; i++)
-        {
-            Vector3 A = SamplePoints[i];
-            Vector3 B = SamplePoints[i+1];
+        for (int i = 0; i < BakedPoints.Count - 1; i++) {
+            Vector3 A = BakedPoints[i];
+            Vector3 B = BakedPoints[i + 1];
 
             Vector3 AB = B - A;
             Vector3 AP = P - A;
             float t01 = Mathf.Clamp01(Vector3.Dot(AP, AB) / Vector3.Dot(AB, AB));
             Vector3 C = A + t01 * AB;
             float d = (P - C).magnitude;
-            if (d < closestDistance){
+            if (d < closestDistance) {
                 closestDistance = d;
-                ClosestPoint = C;
-                ClosestIndex = i;
-                sA = A;
-                sB = B;
+                ClosestPoint = new BezierPoint(this, C, i);
             }
         }
-	}
+        return ClosestPoint;
+    }
 
-	private void OnDrawGizmos()
-	{
+    private void OnDrawGizmos() {
         if (Points.Count < 4) return;
 
         // handles
-        for (int i = 0; i < Points.Count - 3; i+=3) {
+        for (int i = 0; i < Points.Count - 3; i += 3) {
             Vector3 p0 = Points[i].position;
             Vector3 p1 = Points[i + 1].position;
             Vector3 p2 = Points[i + 2].position;
@@ -74,12 +75,13 @@ public class Bezier : MonoBehaviour {
                 Gizmos.DrawLine(p2, p3);
             }
 
-            Gizmos.color = Color.black;
+            Gizmos.color = Color.yellow;
             Vector3 prev = p0;
-            for (float j = 1; j <= 10; j++)
-            {
+            Gizmos.DrawSphere(p0, 0.2f);
+            for (float j = 1; j <= 10; j++) {
                 Vector3 p = BezierSegmentQuad(p0, p1, p2, p3, j / 10);
                 Gizmos.DrawLine(prev, p);
+                Gizmos.DrawSphere(p, 0.1f);
                 prev = p;
             }
         }
@@ -90,19 +92,19 @@ public class Bezier : MonoBehaviour {
         Gizmos.color = Color.green;
         Gizmos.DrawSphere(Points[0].position, .2f);
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(Points[Points.Count-1].position, .2f);
-
+        Gizmos.DrawSphere(Points[Points.Count - 1].position, .2f);
 
 
         // closest point
-        Gizmos.color = Color.yellow;
-        for (int i = 0; i < SamplePoints.Count; i++)
-        {
-            Gizmos.DrawSphere(SamplePoints[i], .1f);
-            if (i < SamplePoints.Count-1)
-                Gizmos.DrawLine(SamplePoints[i], SamplePoints[i + 1]);
+        if (Application.isPlaying) {
+            Gizmos.color = Color.magenta;
+            for (int i = 0; i < BakedPoints.Count; i++) {
+                Gizmos.DrawSphere(BakedPoints[i], .1f);
+                if (i < BakedPoints.Count - 1)
+                    Gizmos.DrawLine(BakedPoints[i], BakedPoints[i + 1]);
+            }
+            Gizmos.DrawSphere(ClosestPoint.Position, .3f);
         }
-        Gizmos.DrawSphere(ClosestPoint, .1f);
     }
 
     public Vector3 PointOnBezier(float t) {
@@ -127,8 +129,7 @@ public class Bezier : MonoBehaviour {
     }
 
 
-    private Vector3 BezierSegmentQuad(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
-    {
+    private Vector3 BezierSegmentQuad(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) {
         Vector3 pA = BezierSegment(p0, p1, p2, t);
         Vector3 pB = BezierSegment(p1, p2, p3, t);
         Vector3 p = Vector3.Lerp(pA, pB, t);
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,30 +3,157 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-
+    [Header("Refs")]
+    [SerializeField] private Camera Camera;
+    [SerializeField] private Transform CharTransform;
     [SerializeField] private Transform CameraTarget = null;
-    [SerializeField] private float CameraDistance = 5;
+    [Header("Settings")]
     [SerializeField] private Vector2 CameraSpeed = new Vector2(.3f, .1f);
-    [SerializeField] private Vector2 MinMaxAngle = new Vector2(30, 60);
     [SerializeField] private float MouseSensitivity = 10;
+    [Header("Rails")]
+    [SerializeField] private List<Bezier> Rails;
+    [SerializeField] private bool UseRails = false;
+    [SerializeField] private bool RailOrTunnel = true;
+    [SerializeField] private AnimationCurve RailTransitionCurve;
+    [SerializeField] private float RailFadeRadius = 5;
+    [SerializeField] private float RailFadeBuffer = 1;
+    [SerializeField] private float CameraDistance = 5;
+    [SerializeField] private float RailCameraDistance = 5;
+    [SerializeField] private float FOV = 30;
+    [SerializeField] private float TunnelFOV = 90;
+    [SerializeField] private Vector2 MinMaxAngle = new Vector2(30, 60);
+    [SerializeField] private Vector2 TunnelMinMaxAngle = new Vector2(-20, 10);
+    [SerializeField] private float RailFadeTransitionDuration = 0.5f;
 
     private Vector3 TargetPos;
-    private Vector3 CameraRotation = Vector3.zero;
+    private Vector3 FreeCamRotation = Vector3.zero;
+    private float ClosestDistance;
+    private BezierPoint ClosestPoint;
+    private float RailFadeT = 0;
 
-    void FixedUpdate () {
+    private void Start() {
+        foreach (var rail in Rails) {
+            rail.BakeBezier();
+        }
+    }
+
+    void Update() {
         TargetPos = CameraTarget.position;
 
+        // free camera position calculation
+        Ray freeCamRay = FreeCam();
+        Vector3 freeCamPos = freeCamRay.GetPoint(0);
+        Quaternion freeCamRot = Quaternion.LookRotation(freeCamRay.direction);
+
+        // rail camera position calculation
+        Ray railCamRay = RailOrTunnel ? RailCam() : TunnelCam();
+        Vector3 railCamPos = railCamRay.GetPoint(0);
+        Quaternion railCamRot = Quaternion.LookRotation(railCamRay.direction);
+
+
+        // apply values
+        float t01 = GetT01TimeSwitchBased();
+        Vector3 finalPos = Vector3.Lerp(freeCamPos, railCamPos, UseRails ? RailTransitionCurve.Evaluate(t01) : 0);
+        Quaternion finalRot = Quaternion.Lerp(freeCamRot, railCamRot, UseRails ? t01 : 0);
+        transform.rotation = finalRot;
+        transform.position = finalPos;
+    }
+
+    private float GetT01PositionBased() {
+        return Mathf.InverseLerp(RailFadeRadius + RailFadeBuffer, RailFadeRadius, ClosestDistance);
+    }
+
+    private float GetT01TimeSwitchBased() {
+        bool closerToRail = ClosestDistance < RailFadeRadius;
+        RailFadeT += Time.deltaTime * (closerToRail ? 1.0f : -1.0f);
+        RailFadeT = Mathf.Clamp(RailFadeT, 0.0f, RailFadeTransitionDuration);
+        float t01 = RailFadeT / RailFadeTransitionDuration;
+        if (t01 <= 0) {
+            return 0;
+        }
+        if (t01 > 1) {
+            return 1;
+        }
+        return t01;
+    }
+
+    private Ray FreeCam() {
 
         // rot
-        CameraRotation += new Vector3(-Input.GetAxis("Mouse Y") * CameraSpeed.y,
+        FreeCamRotation += new Vector3(-Input.GetAxis("Mouse Y") * CameraSpeed.y,
                                       Input.GetAxis("Mouse X") * CameraSpeed.x) * Time.deltaTime * 60 * MouseSensitivity;
-
-        CameraRotation += new Vector3(Input.GetAxis("RightStickY") * CameraSpeed.y,
+        FreeCamRotation += new Vector3(Input.GetAxis("RightStickY") * CameraSpeed.y,
                                       Input.GetAxis("RightStickX") * CameraSpeed.x) * Time.deltaTime * 60;
+        Vector2 currentMinMaxAngle = Vector2.Lerp(MinMaxAngle, TunnelMinMaxAngle, RailFadeT / RailFadeTransitionDuration);
+        FreeCamRotation.x = Mathf.Clamp(FreeCamRotation.x, currentMinMaxAngle.x, currentMinMaxAngle.y);
+        transform.eulerAngles = FreeCamRotation;
 
-        CameraRotation.x = Mathf.Clamp(CameraRotation.x, MinMaxAngle.x, MinMaxAngle.y);
+        // FOV fade
+        Camera.fieldOfView = Mathf.Lerp(FOV, TunnelFOV, RailFadeT / RailFadeTransitionDuration);
 
-        transform.eulerAngles = CameraRotation;
-        transform.position = TargetPos - transform.forward * CameraDistance;
-	}
+
+        // free camera position calculation
+        Vector3 pos = TargetPos - transform.forward * CameraDistance;
+        Vector3 dir = transform.forward;
+        return new Ray(pos, dir);
+    }
+
+    private void CalculateClosestRailDistance() {
+        // get closest point
+        ClosestDistance = Mathf.Infinity;
+        Bezier closestRail;
+        foreach (Bezier rail in Rails) {
+            BezierPoint cp = rail.GetClosestPoint(TargetPos);
+            float d = Vector3.Distance(cp.Position, TargetPos);
+            if (d < ClosestDistance) {
+                ClosestDistance = d;
+                closestRail = rail;
+                ClosestPoint = cp;
+            }
+        }
+    }
+
+    private Ray RailCam() {
+        CalculateClosestRailDistance();
+
+        // rail alignment
+        Vector3 segmentDir = (ClosestPoint.SegmentP1 - ClosestPoint.SegmentP0).normalized;
+        int segmentAlligned = 1;// Vector3.Dot(CharTransform.forward, railSegmentDir) > 0 ? 1 : -1;
+
+        Debug.Log(string.Format("ClosestDistance:{0}, inverseLerp:{1}, RailFadeT:{2}", ClosestDistance, Mathf.InverseLerp(5, 2, ClosestDistance), RailFadeT));
+        Vector3 pos = ClosestPoint.Position - segmentDir * RailCameraDistance * segmentAlligned;
+        Vector3 dir = segmentDir * segmentAlligned;
+        return new Ray(pos, dir);
+    }
+
+    private Ray TunnelCam() {
+        CalculateClosestRailDistance();
+
+        // rail alignment
+        Vector3 pos = TargetPos - transform.forward * RailCameraDistance;
+        Vector3 dir = transform.forward;
+        return new Ray(pos, dir);
+    }
+
+    private void OnDrawGizmos() {
+        if (Application.isPlaying) {
+            Vector3 railSegmentDir = (ClosestPoint.SegmentP1 - ClosestPoint.SegmentP0).normalized;
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawRay(TargetPos, railSegmentDir);
+
+            // draw fade radius
+            Color color = Color.magenta;
+            foreach (var rail in Rails) {
+                foreach (Vector3 p in new List<Vector3>(){
+                rail.GetPoints[0].position,
+                rail.GetPoints[rail.GetPoints.Count-1].position }
+                ) {
+                    Gizmos.color = color;
+                    Gizmos.DrawWireSphere(p, RailFadeRadius);
+                    Gizmos.color = color * .75f;
+                    Gizmos.DrawWireSphere(p, RailFadeRadius + RailFadeBuffer);
+                }
+            }
+        }
+    }
 }
